845aada32cc59a83436fe6b8ef666b2e4453d6ef
Sourav-cpu-crypto <souravmukherjeemca1@gmail.com>
2021-12-04 00:14:21 +0530

    15th

--- src/Layout/RoutingModule/Search.js
+++ src/Layout/RoutingModule/Search.js
@@ -18,38 +18,7 @@ export const Search = (props) => {
 
     return (
         <div>
-         <Carousel variant="dark" margin-top='90px'>
-  <Carousel.Item>
-    <img
-      className="d-block w-100" width='480px' height='540px'
-      src="https://s3.amazonaws.com/pbblogassets/uploads/2019/10/11123757/shutterstock_686692210-1.jpg"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-    
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"  width='480px' height='480px'
-      src="https://th.bing.com/th/id/R.d2a0edf6683f7a5012d7c72e6f8dfd30?rik=mkNcvtoJaaWkQA&riu=http%3a%2f%2fnewwallpapershd.com%2fwp-content%2fuploads%2f2015%2f01%2fBing-Bang-3D-video-Games-HD-wallpapers.jpg&ehk=Anmt8%2f0AtohzgDtaS1zFt55Mh77NXzOYe%2bmN5Gac%2boo%3d&risl=&pid=ImgRaw&r=0"
-      alt="Second slide"
-    />
-    <Carousel.Caption>
   
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"   width='480px' height='480px'
-      src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-      alt="Third slide"
-    />
-    <Carousel.Caption>
-   
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
 
 
     
